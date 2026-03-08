ad17d0c3b9feaa8202211f0a8816bb74b46c67a6
wasabi315 <wasabi.nvim.st@gmail.com>
2026-03-08 11:57:08 +0900

    Add index for name similarity search

--- migration/202603071213_init.sql
+++ migration/202603071213_init.sql
@@ -10,5 +10,6 @@ CREATE TABLE library_items (
     body                  bytea                      -- NULL for axioms
 );
 
+CREATE INDEX items_name_unqual_trgm          ON library_items USING GIST (name_unqual gist_trgm_ops);
 CREATE INDEX items_used_top_names_qual_gin   ON library_items USING gin (used_top_names_qual);
 CREATE INDEX items_used_top_names_unqual_gin ON library_items USING gin (used_top_names_unqual);
